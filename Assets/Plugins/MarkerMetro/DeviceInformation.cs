0707131810a2de638c0939077a9d346a5ca33ad5
Greg Quinn <greg.q@markermetro.com>
2014-05-06 16:34:36 +1200

    Added DeviceInformation class

---
+++ Assets/Plugins/MarkerMetro/DeviceInformation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public static class DeviceInformation
+{
+    public static Func<Environment> DoGetEnvironment;
+
+    public static Environment GetEnvironment()
+    {
+        if (DoGetEnvironment != null)
+            return DoGetEnvironment();
+        else
+            return Environment.Dev;
+    }
+
+    public enum Environment
+    {
+        Dev,
+        QA,
+        Production
+    }
+}
