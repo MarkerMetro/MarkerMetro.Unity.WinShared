9b8ad151dd1eb22634df02f0850a182b39a5dcc4
Roberto Caldas <roberto.c@markermetro.com>
2014-06-10 11:25:49 +1200

    Created a script that tries to apply a DTX5 compression to a texture when importing it.

---
+++ Assets/Editor/MarkerMetro/DTX5TexturePostprocessor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+/**
+ * Tries to apply a DXT5 compression when importing a texture.
+ * This will change the texture meta file.
+ */
+class DTX5TexturePostprocessor : AssetPostprocessor {
+
+    // ENABLE HERE TO MAKE THE SCRIPT WORK
+    const bool Enabled = false;
+
+#pragma warning disable
+
+    public void OnPostprocessTexture (Texture2D t) {
+        if(Enabled)
+            EditorUtility.CompressTexture(t, TextureFormat.DXT5, 2); 
+    } 
+#pragma warning restore
+} 
