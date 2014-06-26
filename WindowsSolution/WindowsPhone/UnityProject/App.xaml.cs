46673a1c5cdd8cf01683d640a01caa12dda150ab
Keith Patton <keith.p@markermetro.com>
2014-06-26 15:15:43 +1200

    fix for WP8 to ensure app class namespace name matches unity product name in player settings

--- WindowsSolution/WindowsPhone/UnityProject/App.xaml.cs
+++ WindowsSolution/WindowsPhone/UnityProject/App.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 
-namespace UnityProject.WinPhone
+namespace UnityProject
 {
 	public partial class App : Application
 	{
