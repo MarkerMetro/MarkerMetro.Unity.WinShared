9d32dfcd2e990760649d07c5c571d02215ada79b
Chad Edwards <chad.e@markermetro.com>
2014-08-21 12:37:43 +1200

    Updated live tile to cycle through front and back

--- WindowsSolution/WindowsStore/UnityProject/MainPage.xaml.cs
+++ WindowsSolution/WindowsStore/UnityProject/MainPage.xaml.cs
@@ -173,48 +173,105 @@ namespace UnityProject.Win
 
         void UpdateLiveTiles()
         {
-            var content = TileUpdateManager.GetTemplateContent(TileTemplateType.TileSquare150x150Text01);
-            var contentWide = TileUpdateManager.GetTemplateContent(TileTemplateType.TileWide310x150SmallImageAndText02);
-            var resources = ResourceLoader.GetForViewIndependentUse();
-
-            Debug.WriteLine(content.GetXml());
-            Debug.WriteLine(contentWide.GetXml());
-
             try
             {
-                var wideBinding = (XmlElement)contentWide.GetElementsByTagName("binding").Single();
-                wideBinding.SetAttribute("branding", "name");
+                //***********************************************
+                //Adjust these parameters with appropriate values
+                string frontMediumImagePath = "/Assets/Logo.png";
+                string frontWideImagePath = "/Assets/WideLogo.png";
+                string backWideImagePath = "/Assets/Logo.png";
 
-                var xTexts = content.GetElementsByTagName("text");
-                var xWideTexts = wideBinding.GetElementsByTagName("text");
-                
-                xWideTexts[0].InnerText = xTexts[0].InnerText = "UnityProject"; // update with localized game value
-                xWideTexts[1].InnerText = xTexts[1].InnerText = "UnityProject"; // update with localized game value
-                xWideTexts[2].InnerText = xTexts[2].InnerText = "UnityProject"; // update with localized game value
+                string mediumText1 = "Level"; // Don't forget to localise
+                string mediumText2 = "Score";
+                string mediumText3 = "Time";
+                string mediumText4 = "Coins";
 
-                xTexts[3].InnerText = "UnityProject"; // update with localized game value
-                xWideTexts[3].InnerText = "UnityProject"; // update with localized game value
+                string mediumData1 = "99"; // This data should come from a game class
+                string mediumData2 = "1234";
+                string mediumData3 = "11:22";
+                string mediumData4 = "7";
 
-                var xImage = wideBinding.GetElementsByTagName("image");
-                ((XmlElement)xImage[0]).SetAttribute("src", "/Assets/WideLogo.png");
-                //((XmlElement)xImage[1]).SetAttribute("src", "/Assets/Logo.png");
+                string wideText1 = "Highest Level"; // Wide tile can only display three lines of text
+                string wideText2 = "High Score";
+                string wideText3 = "Total Time";
 
-                var xVisual = (XmlElement)content.GetElementsByTagName("visual").Single();
-                xVisual.AppendChild(content.ImportNode(wideBinding, true));
-                Debug.WriteLine(content.GetXml());
+                string wideData1 = "99";
+                string wideData2 = "1234";
+                string wideData3 = "11:22";
+                //
+                //***********************************************
 
-                var notificiation = new TileNotification(content)
-                {
-                    Tag = content.GetHashCode().ToString(),
-                };
+                // Retrieve the XML that defines the appearance of the tiles
+                XmlDocument frontMediumTemplate = TileUpdateManager.GetTemplateContent(TileTemplateType.TileSquare150x150Image);
+                XmlDocument backMediumTemplate = TileUpdateManager.GetTemplateContent(TileTemplateType.TileSquare150x150Text01);
+                XmlDocument frontWideTemplate = TileUpdateManager.GetTemplateContent(TileTemplateType.TileWide310x150Image);
+                XmlDocument backWideTemplate = TileUpdateManager.GetTemplateContent(TileTemplateType.TileWide310x150SmallImageAndText02);
 
-                TileUpdateManager.CreateTileUpdaterForApplication().Update(notificiation);
+                // Tiles should display game name on the back, but not on the front, as it will be in the image
+                // retrieve the 'binding' element from the xml
+                XmlElement frontMediumBinding = (XmlElement)frontMediumTemplate.GetElementsByTagName("binding").Single();
+                XmlElement backMediumBinding = (XmlElement)backMediumTemplate.GetElementsByTagName("binding").Single();
+                XmlElement frontWideBinding = (XmlElement)frontWideTemplate.GetElementsByTagName("binding").Single();
+                XmlElement backWideBinding = (XmlElement)backWideTemplate.GetElementsByTagName("binding").Single();
+
+                // Set 'branding' attribute on 'binding' element
+                // fronts need 'none' backs need 'name'
+                frontMediumBinding.SetAttribute("branding", "none");
+                backMediumBinding.SetAttribute("branding", "name");
+                frontWideBinding.SetAttribute("branding", "none");
+                backWideBinding.SetAttribute("branding", "name");
+
+                // Set 'src' attribute in 'image' elements
+                // Both front tiles need images
+                // only wide tile has image on back
+                // Access elements
+                XmlElement frontMediumImage = (XmlElement)frontMediumTemplate.GetElementsByTagName("image").Single();
+                XmlElement frontWideImage = (XmlElement)frontWideTemplate.GetElementsByTagName("image").Single();
+                XmlElement backWideImage = (XmlElement)backWideTemplate.GetElementsByTagName("image").Single();
+                // Set attributes
+                frontMediumImage.SetAttribute("src", frontMediumImagePath);
+                frontWideImage.SetAttribute("src", frontWideImagePath);
+                backWideImage.SetAttribute("src", backWideImagePath);
+
+                // Set lines of text to appear on back of tiles.
+                // Retrieve lists of text elements
+                XmlNodeList mediumTextList = backMediumTemplate.GetElementsByTagName("text");
+                XmlNodeList wideTextList = backWideTemplate.GetElementsByTagName("text");
+                // Set text for each element
+                mediumTextList[0].InnerText = string.Format("{0}: {1}", mediumText1, mediumData1);
+                mediumTextList[1].InnerText = string.Format("{0}: {1}", mediumText2, mediumData2);
+                mediumTextList[2].InnerText = string.Format("{0}: {1}", mediumText3, mediumData3);
+                mediumTextList[3].InnerText = string.Format("{0}: {1}", mediumText4, mediumData4);
+                wideTextList[0].InnerText = string.Format("{0}: {1}", wideText1, wideData1);
+                wideTextList[1].InnerText = string.Format("{0}: {1}", wideText2, wideData2);
+                wideTextList[2].InnerText = string.Format("{0}: {1}", wideText3, wideData3);
+
+                // Each notification requires the data for the wide and medium tiles
+                // One notification for the front, and one for the back
+                // Join the templates.
+                // Access 'visual' element of medium which contains medium 'binding' element
+                XmlElement frontVisual = frontMediumTemplate.GetElementsByTagName("visual").Single();
+                XmlElement backVisual = backMediumTemplate.GetElementsByTagName("visual").Single();
+                //add 'wide' binding element to it
+                frontVisual.AppendChild(frontMediumTemplate.ImportNode(frontWideBinding, true)); 
+                backVisual.AppendChild(backMediumTemplate.ImportNode(backWideBinding, true));
+
+                TileNotification frontNotification = new TileNotification(frontMediumTemplate); // Both contains medium and wide after join.
+                TileNotification BackNotification = new TileNotification(backMediumTemplate); 
+
+                TileUpdater tileUpdater = TileUpdateManager.CreateTileUpdaterForApplication();  // Access tile updater
+                tileUpdater.EnableNotificationQueue(true);                                      // Enable queing to cycle through front and back
+                tileUpdater.Clear();                                                            // Make sure queue is empty
+                tileUpdater.Update(BackNotification);                                           // Switch to back tile
+                tileUpdater.Update(frontNotification);                                          // Queue up front tile
+
+                // Reading the actual Xml help to understand what is happening here.
+                Debug.WriteLine(frontMediumTemplate.GetXml());
+                Debug.WriteLine(backMediumTemplate.GetXml());
             }
             catch (Exception ex)
             {
                 Debug.WriteLine(ex);
-
-                MarkerMetro.Unity.WinIntegration.ExceptionLogger.Instance.Send(ex);
             }
         }
 
