3bd07b8f3a8dd7b6d5f9613b1bc8696ec9d985bb
Freddy Weng <freddy.w@markermetro.com>
2014-12-11 12:20:04 +1300

    Added "MarkerMetro/Plugins/Refresh from Nuget", attemp to execute the nuget bat file from Unity Editor

---
+++ Assets/Editor/MarkerMetro/MarkerMetroPlugins.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.Collections;
+using System.Diagnostics;
+
+public static class MarkerMetroPlugins
+{
+    /// <summary>
+    /// Attempt to execute the Update_NuGet_Packages.bat file.
+    /// Show any errors/success in a message box.
+    /// </summary>
+    [MenuItem("MarkerMetro/Plugins/Refresh from Nuget")]
+    public static void RefreshFromNuget()
+    {
+        string cmdPath = "cmd.exe";
+        string nugetUpdateDir = Application.dataPath + "\\..\\NuGet\\";
+        string nugetUpdateFilename = "Update_NuGet_Packages.bat";
+
+        Process process = null;
+
+        try {
+            process = new Process();
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.FileName = cmdPath;
+            process.StartInfo.WorkingDirectory = nugetUpdateDir;
+            process.StartInfo.Arguments = "/c " + nugetUpdateFilename;
+            process.EnableRaisingEvents = true;
+            process.Start();
+
+            process.WaitForExit();
+            int exitCode = process.ExitCode;
+            if (exitCode == 0)
+            {
+                EditorUtility.DisplayDialog("Refresh from Nuget", "Refresh from Nuget completed.", "OK");
+            }
+            else
+            {
+                EditorUtility.DisplayDialog("Refresh from Nuget", "Refresh from Nuget failed with exit code: " + exitCode, "OK");
+            }
+        } 
+        catch (Exception e)
+        {
+            EditorUtility.DisplayDialog("Refresh from Nuget", "Exception: " + e, "OK");
+        }
+        finally
+        {
+            if (process != null)
+                process.Close();
+        }
+    }
+}
