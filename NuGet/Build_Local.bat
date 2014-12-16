12722635fa3f74b69a0a88195c42542112336274
Freddy Weng <freddy.w@markermetro.com>
2014-12-16 17:39:10 +1300

    Fix local build

--- NuGet/Build_Local.bat
+++ NuGet/Build_Local.bat
@@ -20,18 +20,18 @@ msbuild "%winIntegrationDir%\%winIntegration%Metro\%winIntegration%Metro.csproj"
 msbuild "%winIntegrationDir%\%winIntegration%Unity\%winIntegration%Unity.csproj" /p:Configuration=%buildConfig% /t:Clean;Build
 msbuild "%winIntegrationDir%\%winIntegration%WP8\%winIntegration%WP8.csproj" /p:Configuration=%buildConfig% /t:Clean;Build
 
-xcopy /y "%winLegacyDir%\%winLegacy%Metro\bin\%buildConfig%\%winLegacy%.dll" "%unityPluginDir%\Metro\%winLegacy%.dll"
-xcopy /y "%winLegacyDir%\%winLegacy%Unity\bin\%buildConfig%\%winLegacy%.dll" "%unityPluginDir%\%winLegacy%.dll"
-xcopy /y "%winLegacyDir%\%winLegacy%WP8\bin\%buildConfig%\%winLegacy%.dll" "%unityPluginDir%\WP8\%winLegacy%.dll"
+copy /y "%winLegacyDir%\%winLegacy%Metro\bin\%buildConfig%\%winLegacy%.dll" "%unityPluginDir%\Metro\%winLegacy%.dll"
+copy /y "%winLegacyDir%\%winLegacy%Unity\bin\%buildConfig%\%winLegacy%.dll" "%unityPluginDir%\%winLegacy%.dll"
+copy /y "%winLegacyDir%\%winLegacy%WP8\bin\%buildConfig%\%winLegacy%.dll" "%unityPluginDir%\WP8\%winLegacy%.dll"
 
-xcopy /y "%winLegacyDir%\%winLegacy%Metro\bin\%buildConfig%\%winLegacy%.pdb" "%unityPluginDir%\Metro\%winLegacy%.pdb"
-xcopy /y "%winLegacyDir%\%winLegacy%Unity\bin\%buildConfig%\%winLegacy%.pdb" "%unityPluginDir%\%winLegacy%.pdb"
-xcopy /y "%winLegacyDir%\%winLegacy%WP8\bin\%buildConfig%\%winLegacy%.pdb" "%unityPluginDir%\WP8\%winLegacy%.pdb"
+copy /y "%winLegacyDir%\%winLegacy%Metro\bin\%buildConfig%\%winLegacy%.pdb" "%unityPluginDir%\Metro\%winLegacy%.pdb"
+copy /y "%winLegacyDir%\%winLegacy%Unity\bin\%buildConfig%\%winLegacy%.pdb" "%unityPluginDir%\%winLegacy%.pdb"
+copy /y "%winLegacyDir%\%winLegacy%WP8\bin\%buildConfig%\%winLegacy%.pdb" "%unityPluginDir%\WP8\%winLegacy%.pdb"
 
-xcopy /y "%winIntegrationDir%\%winIntegration%Metro\bin\%buildConfig%\%winIntegration%.dll" "%unityPluginDir%\Metro\%winIntegration%.dll"
-xcopy /y "%winIntegrationDir%\%winIntegration%Unity\bin\%buildConfig%\%winIntegration%.dll" "%unityPluginDir%\%winIntegration%.dll"
-xcopy /y "%winIntegrationDir%\%winIntegration%WP8\bin\%buildConfig%\%winIntegration%.dll" "%unityPluginDir%\WP8\%winIntegration%.dll"
+copy /y "%winIntegrationDir%\%winIntegration%Metro\bin\%buildConfig%\%winIntegration%.dll" "%unityPluginDir%\Metro\%winIntegration%.dll"
+copy /y "%winIntegrationDir%\%winIntegration%Unity\bin\%buildConfig%\%winIntegration%.dll" "%unityPluginDir%\%winIntegration%.dll"
+copy /y "%winIntegrationDir%\%winIntegration%WP8\bin\%buildConfig%\%winIntegration%.dll" "%unityPluginDir%\WP8\%winIntegration%.dll"
 
-xcopy /y "%winIntegrationDir%\%winIntegration%Metro\bin\%buildConfig%\%winIntegration%.pdb" "%unityPluginDir%\Metro\%winIntegration%.pdb"
-xcopy /y "%winIntegrationDir%\%winIntegration%Unity\bin\%buildConfig%\%winIntegration%.pdb" "%unityPluginDir%\%winIntegration%.pdb"
-xcopy /y "%winIntegrationDir%\%winIntegration%WP8\bin\%buildConfig%\%winIntegration%.pdb" "%unityPluginDir%\WP8\%winIntegration%.pdb"
+copy /y "%winIntegrationDir%\%winIntegration%Metro\bin\%buildConfig%\%winIntegration%.pdb" "%unityPluginDir%\Metro\%winIntegration%.pdb"
+copy /y "%winIntegrationDir%\%winIntegration%Unity\bin\%buildConfig%\%winIntegration%.pdb" "%unityPluginDir%\%winIntegration%.pdb"
+copy /y "%winIntegrationDir%\%winIntegration%WP8\bin\%buildConfig%\%winIntegration%.pdb" "%unityPluginDir%\WP8\%winIntegration%.pdb"
