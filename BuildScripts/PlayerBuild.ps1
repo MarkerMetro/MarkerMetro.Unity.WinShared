4fdf77867166b05aaff34552384581bfea684f15
Roberto Caldas <roberto.c@markermetro.com>
2015-02-16 20:33:31 -0200

    Add default case in switch to throw error

--- BuildScripts/PlayerBuild.ps1
+++ BuildScripts/PlayerBuild.ps1
@@ -13,63 +13,68 @@ param(
 
 Try 
 {
-if(Test-Path $projectPath\logs)
-{
- rmdir $projectPath\logs -Force -Recurse | Out-Null
-}
-mkdir $projectPath\logs | Out-Null
-
-if(!(Test-Path "$unityPath"))
-{
-   throw "Unity executable was not found at $unityPath"
-}
-
-$methodName = "Assets.Editor.MarkerMetro.MarkerMetroBuilder."
-
-Switch ($platform)
-{
-	"WindowsStore" 
+	if(Test-Path $projectPath\logs)
 	{
-		$methodName += "BuildMetro"
-		$displayName = "Windows Store"
-		$buildTarget = "Metro"
+	 rmdir $projectPath\logs -Force -Recurse | Out-Null
 	}
-	
-	"WindowsPhone"
+	mkdir $projectPath\logs | Out-Null
+
+	if(!(Test-Path "$unityPath"))
 	{
-		$methodName += "BuildWP8"
-		$displayName = "Windows Phone"
-		$buildTarget = "WP8"
+	   throw "Unity executable was not found at $unityPath"
 	}
-	
-	"WindowsUniversal"
+
+	$methodName = "Assets.Editor.MarkerMetro.MarkerMetroBuilder."
+
+	Switch ($platform)
 	{
-		$methodName += "BuildUniversal"
-		$displayName = "Windows Universal 8.1"	
-		$buildTarget = "Metro"			
+		"WindowsStore" 
+		{
+			$methodName += "BuildMetro"
+			$displayName = "Windows Store"
+			$buildTarget = "Metro"
+		}
+		
+		"WindowsPhone"
+		{
+			$methodName += "BuildWP8"
+			$displayName = "Windows Phone"
+			$buildTarget = "WP8"
+		}
+		
+		"WindowsUniversal"
+		{
+			$methodName += "BuildUniversal"
+			$displayName = "Windows Universal 8.1"	
+			$buildTarget = "Metro"			
+		}
+		
+		default
+		{
+			throw "Invalid platform $platform"
+		}
 	}
-}
 
-& "$unityPath" -buildTarget $buildTarget -projectPath $projectPath -batchmode -quit -logFile $projectPath\logs\$methodName.log -executeMethod $methodName -CustomArgs:outputPath=$outputPath | Out-Null
+	& "$unityPath" -buildTarget $buildTarget -projectPath $projectPath -batchmode -quit -logFile $projectPath\logs\$methodName.log -executeMethod $methodName -CustomArgs:outputPath=$outputPath | Out-Null
 
-$result = $?
+	$result = $?
 
-Get-Content $projectPath\logs\$methodName.log | Out-Default
+	Get-Content $projectPath\logs\$methodName.log | Out-Default
 
-if(!((Get-Content $projectPath\logs\$methodName.log -Tail 10).Contains("Exiting batchmode successfully now!"))){
-	throw "Batch Mode did not complete successfully"
-}
+	if(!((Get-Content $projectPath\logs\$methodName.log -Tail 10).Contains("Exiting batchmode successfully now!"))){
+		throw "Batch Mode did not complete successfully"
+	}
 
-if(!$result){
-  Write-Host "##teamcity[message text='$displayName Player build failed' errorDetails='The process exited with a non-zero exit code. Check the logs for details' status='FAILURE']"
-}
+	if(!$result){
+	  Write-Host "##teamcity[message text='$displayName Player build failed' errorDetails='The process exited with a non-zero exit code. Check the logs for details' status='FAILURE']"
+	}
 
-if(!$result){
-  throw "$displayName Player Build Failed"
-}
+	if(!$result){
+	  throw "$displayName Player Build Failed"
+	}
 }
 Finally
 {
-Write-Host "##teamcity[blockClosed name='Building $displayName Player Build']"
+	Write-Host "##teamcity[blockClosed name='Building $displayName Player Build']"
 	exit($lastexitcode)
 }
