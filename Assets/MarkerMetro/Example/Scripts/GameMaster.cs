8577e8c5a90d736fe74b932329026afe6cdf15a8
Freddy Weng <hungweng@gmail.com>
2015-03-30 17:39:52 +1300

    Add WP8.1 specific plugins

--- Assets/MarkerMetro/Example/Scripts/GameMaster.cs
+++ Assets/MarkerMetro/Example/Scripts/GameMaster.cs
@@ -564,6 +564,7 @@ namespace MarkerMetro.Unity.WinShared.Example
             }
         }
 
+#if (UNITY_WP8 || UNITY_WP_8_1) && !UNITY_EDITOR
         private IEnumerator SetFBStatus(FBUser user)
         {
             FacebookName = user.Name;
@@ -571,6 +572,7 @@ namespace MarkerMetro.Unity.WinShared.Example
             FacebookImage = new Texture2D(128, 128, TextureFormat.DXT1, false);
             yield return StartCoroutine(GetFBPicture(user.Id, FacebookImage));
         }
+#endif
 
         // Request the players friends.
         // As per FB API v2.0 You can only request friends that have installed and logged in on the app, 
